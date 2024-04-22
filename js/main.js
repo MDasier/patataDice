4eb66a9f8c7e3234753518485e694177f152044d
Asier MD <asier.md09@gmail.com>
2024-04-22 10:58:51 +0200

    Estructura principal hecha, recursos añadidos, lista de TODOs creada

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+//TODO modificacion DOM 
+    //* Acceder a los elementos del DOM y modificarlos, crear nuevos...
+    //? Gestionar los eventos (Pulsa, clica..)
+
+//TODO gestion de clases
+    //* Gestion de métodos y funciones
+
+//TODO intervalos, timeouts y bucles
+    //* Control del juego mediante intervalos y acciones del usuario
+
+//TODO animaciones patataDice (colores, tiempos...)
+    //* Conseguir imagenes y sonidos para las patatas
+    //! HAY QUE DECIDIR COMO MOSTRAR LAS PATATAS EN LA PANTALLA:
+    //? TAMAÑOS-POSICION. APARECER-DESVANCER. ESTATICAS-DINAMICAS. COLORES-BRILLOS.
+
+
+
+
+//TODO Controlar los botones del ordenador (Cual es pulsable cada vez)
+    //* Controlar que no se pueda "apagar" si no está "encendido"
+
+//TODO Encender ordenador:
+    //*mostrar opciones: Empezar a jugar, cambiar dificultad, opciones de sonido
+//TODO Apagar ordenador: 
+    //*apagar pantalla, cerrar bucles/intervalos guardando partida, apagar luces de encendido y encender las de apagado
+
+    //? AL FINAL
+    //TODO gestion de sonido. Musica, sonidos de interaccion usuario, errores...
+    //* Conseguir sonidos para las acciones, menús y música de fondo del juego
+//! SI TODO VA BIEN --> POST IT pegado al marco que clicas y se abre mostrando como se juega (infojuego)
+//! SI TODO VA BIEN --> control responsividad (controlar el tamaño/forma de pantalla)
+
+
